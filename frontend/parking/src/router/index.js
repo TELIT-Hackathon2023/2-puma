c4bb1c4179dfba17bb438d3342648f300b58b91f
OwOHamper <74879467+OwOHamper@users.noreply.github.com>
2023-11-26 12:00:25 +0100

    Revert "TEs"
    This reverts commit e7b0f6ee8c13747886349f0d7793b767c33c2fba.

--- frontend/parking/src/router/index.js
+++ frontend/parking/src/router/index.js
@@ -15,11 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-<<<<<<< Updated upstream
       component: () => import('../components/Account.vue')
-=======
-      component: () => import('../views/AboutView.vue')
->>>>>>> Stashed changes
     },
     {
       path: "/register",
